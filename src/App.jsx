3bf677cd4efb41fdf798ecd3de3bf3b09c691a03
PashaFromRussia <pashafromrussia63@gmail.com>
2025-08-27 13:48:47 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FootballMap from './components/FootballMap';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.map}>
+      <FootballMap />
+    </div>
+  );
+}
+
+export default App;
